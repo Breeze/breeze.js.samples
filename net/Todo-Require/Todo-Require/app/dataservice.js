5f374f49bf21640a168ff547c03cbb92411d39df
Ward Bell <wardbell@hotmail.com>
2014-03-26 00:35:15 -0700

    Todo-Require (KO) copied from Breeze-IdeaBlade and cleaned

---
+++ net/Todo-Require/Todo-Require/app/dataservice.js
@@ -0,0 +1,148 @@
+/*
+ * The data access service for this app.
+ * It requires the Breeze Labs breeze.savequeuing extension which patches Breeze itself.
+ * Get it from https://github.com/Breeze/breeze.js.labs/blob/master/breeze.savequeuing.js
+ * This extension module doesn't return a value so we don't both providing a parameter for it
+ * in the module defining function.
+ */
+define(['jquery', 'ko', 'breeze', 'logger', 'breeze.savequeuing'], function ($, ko, breeze, logger) {
+    
+
+    var serviceName = 'breeze/todos'; // route to the same origin Web Api controller
+
+    // *** Cross origin service example  ***
+    //var serviceName = 'http://todo.breezejs.com/breeze/todos'; // controller in different origin
+
+    var manager = new breeze.EntityManager(serviceName);
+    manager.enableSaveQueuing(true);
+    addTodoProperties();
+
+    return {
+        addPropertyChangeHandler: addPropertyChangeHandler,
+        createTodo: createTodo,
+        deleteTodo: deleteTodo,
+        getTodos: getTodos,
+        hasChanges: hasChanges,
+        purge: purge,
+        reset: reset,
+        saveChanges: saveChanges
+    };
+
+    /*** implementation details ***/
+
+    function addPropertyChangeHandler(handler) {
+        // call handler when an entity property of any entity changes
+        return manager.entityChanged.subscribe(function (changeArgs) {
+            var action = changeArgs.entityAction;
+            if (action === breeze.EntityAction.PropertyChange) {
+                handler(changeArgs);
+            }
+        });
+    }
+
+    function addTodoProperties() {
+        // untracked 'isEditing' property to the 'TodoItem' type
+        // see http://www.breezejs.com/sites/all/apidocs/classes/MetadataStore.html#method_registerEntityTypeCtor
+        var metadataStore = manager.metadataStore;
+        metadataStore.registerEntityTypeCtor('TodoItem', null, todoInit);
+
+        function todoInit(todo) {
+            todo.isEditing = ko.observable(false);
+        }
+    }
+
+    function createTodo(initialValues) {
+        return manager.createEntity('TodoItem', initialValues);
+    }
+
+    function deleteTodo(todoItem) {
+        todoItem && todoItem.entityAspect.setDeleted();
+    }
+
+    function getTodos(includeArchived) {
+        var query = breeze.EntityQuery
+                .from("Todos")
+                .orderBy("CreatedAt");
+
+        if (!includeArchived) { // exclude archived Todos
+            // add filter clause limiting results to non-archived Todos
+            query = query.where("IsArchived", "==", false);
+        }
+
+        return manager.executeQuery(query);
+    }
+
+    function handleSaveValidationError(error) {
+        var message = "Not saved due to validation error";
+        try { // fish out the first error
+            var firstErr = error.entityErrors[0];
+            message += ": " + firstErr.errorMessage;
+        } catch (e) { /* eat it for now */ }
+        return message;
+    }
+
+    function hasChanges() {
+        return manager.hasChanges();
+    }
+
+    function saveChanges() {
+        return manager.saveChanges()
+            .then(saveSucceeded)
+            .fail(saveFailed);
+
+        function saveSucceeded(saveResult) {
+            logger.success("# of Todos saved = " + saveResult.entities.length);
+            logger.log(saveResult);
+        }
+
+        function saveFailed(error) {
+            var reason = error.message;
+            var detail = error.detail;
+
+            if (error.entityErrors) {
+                reason = handleSaveValidationError(error);
+            } else if (detail && detail.ExceptionType &&
+                detail.ExceptionType.indexOf('OptimisticConcurrencyException') !== -1) {
+                // Concurrency error 
+                reason =
+                    "Another user, perhaps the server, " +
+                    "may have deleted one or all of the todos." +
+                    " You may have to restart the app.";
+            } else {
+                reason = "Failed to save changes: " + reason +
+                         " You may have to restart the app.";
+            }
+
+            logger.error(error, reason);
+            // DEMO ONLY: discard all pending changes
+            // Let them see the error for a second before rejecting changes
+            setTimeout(function () {
+                manager.rejectChanges();
+            }, 1000);
+            throw error; // so caller can see it
+        }
+    }
+
+    //#region demo operations
+    function purge(callback) {
+        // Todo: breeze should support commands to the controller
+        // Simplified: fails silently
+        $.post(serviceName + '/purge', function () {
+            logger.success("database purged.");
+            manager.clear();
+            if (callback) callback();
+        });
+    }
+
+    function reset(callback) {
+        // Todo: breeze should support commands to the controller
+        // Simplified: fails silently
+        $.post(serviceName + '/reset', function () {
+            logger.success("database reset.");
+            manager.clear();
+            if (callback) callback();
+        });
+    }
+    //#endregion
+
+});
---
+++ net/Todo-Require/Todo-Require/app/viewModel.js
@@ -0,0 +1,176 @@
+﻿define(['dataservice', 'ko', 'logger', 'Q'], function (dataservice, ko, logger, Q) {
+
+    var vm = {
+        addItem: addItem,
+        archiveCompletedItems: archiveCompletedItems,
+        //archiveCompletedMessage - see addComputed()
+        deleteItem: deleteItem,
+        editBegin: editBegin,
+        editEnd: editEnd,
+        includeArchived: ko.observable(false),
+        items: ko.observableArray(),
+        //itemsLeftMessage - see addComputed()
+        //markAllCompleted - see addComputed()
+        newTodoDescription: ko.observable(""),
+        purge: purge,
+        reset: reset
+    };
+
+    var suspendSave = false;
+
+    initVm();
+
+    return vm; // done with setup; return module variable
+
+    /* Implementation */
+
+    function initVm() {
+        vm.includeArchived.subscribe(getTodos);
+        addComputeds();
+        getTodos();
+
+        // Listen for property change of ANY entity so we can (optionally) save
+        dataservice.addPropertyChangeHandler(propertyChanged);
+    }
+
+    function addComputeds() {
+        vm.archiveCompletedMessage = ko.computed(function () {
+            var count = getStateOfItems().itemsDoneCount;
+            if (count > 0) {
+                return "Archive " + count + " completed item" + (count > 1 ? "s" : "");
+            }
+            return null;
+        });
+
+        vm.itemsLeftMessage = ko.computed(function () {
+            var count = getStateOfItems().itemsLeftCount;
+            if (count > 0) {
+                return count + " item" + (count > 1 ? "s" : "") + " left";
+            }
+            return null;
+        });
+
+        vm.markAllCompleted = ko.computed({
+            read: function () {
+                var state = getStateOfItems();
+                return state.itemsLeftCount === 0 && vm.items().length > 0;
+            },
+            write: function (value) {
+                suspendSave = true;
+                vm.items().forEach(function (item) {
+                    item.IsDone(value);
+                });
+                suspendSave = false;
+                save();
+            }
+        });
+    }
+
+    function archiveCompletedItems() {
+        var state = getStateOfItems();
+        suspendSave = true;
+        state.itemsDone.forEach(function (item) {
+            if (!vm.includeArchived()) {
+                vm.items.remove(item);
+            }
+            item.IsArchived(true);
+        });
+        suspendSave = false;
+        save();
+    }
+
+    function getTodos() {
+        dataservice.getTodos(vm.includeArchived())
+            .then(querySucceeded)
+            .fail(queryFailed);
+
+        function querySucceeded(data) {
+            vm.items(data.results);
+            logger.info("Fetched Todos " +
+                (vm.includeArchived() ? "including archived" : "excluding archived"));
+        }
+        function queryFailed(error) {
+            logger.error(error.message, "Query failed");
+        }
+    }
+
+    function addItem() {
+        var description = vm.newTodoDescription();
+        if (!description) { return; }
+
+        var item = dataservice.createTodo({
+            Description: description,
+            CreatedAt: new Date(),
+            IsDone: vm.markAllCompleted()
+        });
+
+        save(true).catch(addFailed);
+        vm.items.push(item);
+        vm.newTodoDescription("");
+
+        function addFailed() {
+            var index = vm.items.indexOf(item);
+            if (index > -1) {
+                setTimeout(function () { vm.items.splice(index, 1); }, 2000);
+            }
+        }
+    }
+
+    function editBegin(item) { item.isEditing(true); }
+
+    function editEnd(item) { item.isEditing(false); }
+
+    function deleteItem(item) {
+        vm.items.remove(item);
+        dataservice.deleteTodo(item);
+        save(true);
+    }
+
+    function getStateOfItems() {
+        var itemsDone = [], itemsLeft = [];
+
+        vm.items().forEach(function (item) {
+            if (item.IsDone()) {
+                if (!item.IsArchived()) {
+                    itemsDone.push(item); // only unarchived items                
+                }
+            } else {
+                itemsLeft.push(item);
+            }
+        });
+
+        return {
+            itemsDone: itemsDone,
+            itemsDoneCount: itemsDone.length,
+            itemsLeft: itemsLeft,
+            itemsLeftCount: itemsLeft.length
+        };
+    }
+
+    function propertyChanged(changeArgs) {
+        // propertyChanged triggers save attempt UNLESS the property is the 'Id'
+        // because THEN the change is actually the post-save Id-fixup 
+        // rather than user data entry so there is actually nothing to save.
+        if (changeArgs.args.propertyName !== 'Id') {
+            save();
+        }
+    }
+
+    function purge() {
+        return dataservice.purge(getTodos);
+    }
+
+    function reset() {
+        return dataservice.reset(getTodos);
+    }
+
+    function save(force) {
+        // Save if have changes to save AND
+        // if must save OR save not suspended
+        if (dataservice.hasChanges() && (force || !suspendSave)) {
+            return dataservice.saveChanges();
+        }
+        // Decided not to save; return resolved promise w/ no result
+        return Q(false);
+    }
+});
