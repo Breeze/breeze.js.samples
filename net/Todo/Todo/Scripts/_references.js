e72838e938e71fa111f60d3679b1b40106867081
Ward Bell <wardbell@hotmail.com>
2014-03-26 00:05:46 -0700

    Todo (KO) copied from Breeze-IdeaBlade and cleaned

---
+++ net/Todo/Todo/Scripts/_references.js
@@ -0,0 +1,5 @@
+﻿/// <reference path="jquery-1.8.3.js" />
+/// <reference path="knockout-2.2.0.debug.js" />
+/// <reference path="q.js" />
+/// <reference path="toastr.js" />
+/// <reference path="app/viewModel.js" />
