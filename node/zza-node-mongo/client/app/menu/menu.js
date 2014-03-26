2a3a923b0aec6c38b8f4a42caa60226a4f725a25
Ward Bell <wardbell@hotmail.com>
2014-03-26 02:09:52 -0700

    Zza: demonstrating $state.go instead of stateRefs in menu.js and some menus

--- node/zza-node-mongo/client/app/menu/menu.js
+++ node/zza-node-mongo/client/app/menu/menu.js
@@ -3,7 +3,7 @@
 
     angular.module( "app" ).controller( 'menu', controller );
 
-    function controller( $stateParams, dataservice ) {
+    function controller($state, $stateParams, dataservice ) {
         var vm  = this;
         dataservice.ready(onReady);
 
@@ -21,9 +21,20 @@
 
             vm.products = dataservice.products.byTag( type );
             vm.productSref = productSref;
+            vm.go = go;
             vm.template = 'app/menu/menu.' + type + '.html';
         }
 
+        // An ng-click callback that uses $state to navigate
+        // the link url is not visible in the browser and must
+        // style the anchor tag with 'hand' for the cursor to indicate a clickable.
+        // See pizza.html for an example of this approach
+        function go(product) {
+            $state.go('app.order.product', {productType : product.type, productId: product.id});
+        }
+
+        // Generates a link that you can see in the browser
+        // See drink.html for an example of this approach
         function productSref(p) {
             return "app.order.product({productType: '" + p.type + "', productId: '" + p.id +"'})";
             //return '#/menu/'+p.type+'/'+p.id;
