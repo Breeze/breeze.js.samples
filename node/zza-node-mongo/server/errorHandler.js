2310eec87d85cabd87750a6f4ee5e668e05b50f9
Ward Bell <wardbell@hotmail.com>
2014-04-09 14:53:52 -0700

    zza: refactored server routes and database access for clarity, presentation, separation-of-concerns

--- node/zza-node-mongo/server/errorHandler.js
+++ node/zza-node-mongo/server/errorHandler.js
@@ -20,14 +20,29 @@ module.exports = errorHandler;
 
 function errorHandler(err, req, res, next) {
     if (err) {
-        var status = err.statusCode || 500;
         var body =  err.message || err;
-
-        if (process.env.NODE_ENV === 'development'){
-            console.error(status + ' ' + body);
-            if(err.stack) { console.error(err.stack); }
-        }
+        var status = err.statusCode || 500;
         res.send(status, body);
+        logError(err, status, body);
     }
 }
 
+function logError(err, status, body){
+    //todo: get environment variable that says whether to log to console
+    setTimeout(log,0); // let response write to console, then error
+    function log(){
+        var stack = '';
+        var msg = '--------------\n'+status + ' ' + body;
+        // log all inner errors too
+        while(err) {
+            stack = err.stack || stack; // get deepest stack
+            err = err.innerError;
+            if (err && err.message){
+                msg += '\n'+err.message;
+            }
+        }
+        // log deepest stack
+        if(stack) {msg += '\n'+stack; }
+        console.error(msg+'\n--------------');
+    }
+}
