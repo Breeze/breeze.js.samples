3c3aad24329ada3642b0ccd024a66cf81e3f5409
dkoswara <denisk@ideablade.com>
2015-05-20 22:42:47 -0700

    commit getLibs.cmd

--- net/TempHire-Angular/TempHire/getLibs.cmd
+++ net/TempHire-Angular/TempHire/getLibs.cmd
@@ -16,6 +16,7 @@ echo Copying breeze client files into the project
 @echo on
 XCOPY "%1..\..\..\build\libs\js\breeze.min.js" "%1Scripts" /Y
 XCOPY "%1..\..\..\build\libs\js\breeze.debug.js" "%1Scripts" /Y
+XCOPY "%1..\..\..\build\libs\js.adapters\breeze.bridge.angular.js" "%1Scripts" /Y
 
 @echo off
 @echo.
