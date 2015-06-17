3e9c3e3b99932dd64824d84d83d9e30917581db7
Ward Bell <wardbell@hotmail.com>
2015-06-16 17:41:30 -0700

    OData sample: start with empty list

--- net/ODataBreezejsSample/ODataBreezejsSample/Models/TodoListContext.cs
+++ net/ODataBreezejsSample/ODataBreezejsSample/Models/TodoListContext.cs
@@ -21,8 +21,9 @@ namespace ODataBreezejsSample.Models
             {
                 DateTime created = DateTime.UtcNow;
                 var todoLists = new List<TodoList> ();
-                string[] listTitles = { "Work", "Band", "Home", "Wedding", "Other" };
+                string[] listTitles = { "A New List", "Work", "Band", "Home", "Wedding", "Other" };
                 string[][] items = {
+                    new string[] {},
                     new string[] {"Organise 1st 1000 unread emails", "Organise performance review", "Ignore performance review", "Fix coffee machine again"},  
                     new string[] {"Buy new drums", "Sack bass player", "Record album", "Release album", "Dominate world"}, 
                     new string[] {"Fix shower", "Paint decking", "Defrost ice box yet again"}, 
